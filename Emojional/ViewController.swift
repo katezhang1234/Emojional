e6bf89552c27ccfb9397aad429a0c156f9a9349b
Kate Zhang <iamkate1226@gmail.com>
2021-05-08 18:59:31 -0700

    Finished Iteration 2

--- Emojional/ViewController.swift
+++ Emojional/ViewController.swift
@@ -13,7 +13,18 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    
+    let emojis = ["😃": "ugly crying", "😔": "happy", "😤": "pissed", "🤓": "nerdy", "😰": "nervous", "🤡": "clown"]
+    
+    
+    @IBAction func showMessage(sender: UIButton) {
+      //let selectedEmotion = sender.titleLabel?.text
+        let alertController = UIAlertController(title: "Testing 123", message: "Well hello there", preferredStyle: UIAlertController.Style.alert)
+       alertController.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
+       present(alertController, animated: true, completion: nil)
+    }
 
-
+    
+    
 }
-
